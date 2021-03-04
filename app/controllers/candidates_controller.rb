99c6c6f9d702f36b058e5011f33f18043f340a2f
Rafael Luiz <rafelluiz@gmail.com>
2021-03-04 07:54:10 -0300

    Visualizar Candidato apenas se estiver logado
    Existe uma falha que permite qualquer candidato ver informação de outros candidatos
    corrigirei posteriormente.

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,8 +1,16 @@
 class CandidatesController < ApplicationController
-  before_action :authenticate_collaborator!, only: %i[show]
+  before_action :authenticate!, only: %i[show]
 
   def show
     @candidate = Candidate.find(params[:id])
+  end
+
+  private
+
+  def authenticate!
+    unless collaborator_signed_in? or candidate_signed_in?
+      redirect_to root_path, notice: 'Please login as collaborator or candidate!'
+    end
   end
 
 end
